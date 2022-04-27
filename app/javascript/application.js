e3e941d2ad7a5f70cd7f9641d7c322292604794a
Spencer Biri <Cendeu@gmail.com>
2022-04-26 21:57:39 -0500

    make chat scroll and input box refresh on submit

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,12 +8,21 @@ import "./channels/index"
 window.scroll_bottom = function() {
     if ($('#messages').length > 0) {
         $('#messages').scrollTop($('#messages')[0].scrollHeight);
-    }
-}
+    };
+};
+
+const clear_on_submit = function() {
+    document.getElementById('new_message').addEventListener('turbo:submit-start', (e) => {
+        document.getElementById('message_body').value = '';
+        console.log(e);
+    });
+};
 
 
 document.addEventListener("turbo:load", () => {
     $('.ui.dropdown').dropdown();
     $('.message .close').on('click', function() {$(this).closest('.message').transition('fade');});
-    scroll_bottom()
+    scroll_bottom();
+    clear_on_submit();
+
 });
